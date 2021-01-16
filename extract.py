bd906eb6677901d14c705b1c91a148170a6c5d41
Jeannie An <jeanniehjan@gmail.com>
2021-01-16 18:10:32 +0000

    finalised models and extract from files

--- extract.py
+++ extract.py
@@ -1,17 +1,3 @@
-"""Extract data on near-Earth objects and close approaches from CSV and JSON files.
-
-The `load_neos` function extracts NEO data from a CSV file, formatted as
-described in the project instructions, into a collection of `NearEarthObject`s.
-
-The `load_approaches` function extracts close approach data from a JSON file,
-formatted as described in the project instructions, into a collection of
-`CloseApproach` objects.
-
-The main module calls these functions with the arguments provided at the command
-line, and uses the resulting collections to build an `NEODatabase`.
-
-You'll edit this file in Task 2.
-"""
 import csv
 import json
 
@@ -19,20 +5,30 @@ from models import NearEarthObject, CloseApproach
 
 
 def load_neos(neo_csv_path):
-    """Read near-Earth object information from a CSV file.
+    result = []
+    with open(neo_csv_path, mode='r') as csv_file:
+        csv_reader = csv.DictReader(csv_file)
+        line_count = 0
+        for row in csv_reader:
+            pdes = row['pdes'] if row['pdes'] else None
+            name = row['name'] if row['name'] else None
+            diameter = float(
+                row['diameter']) if row['diameter'] else float('nan')
+            pha = True if row['pha'] == 'Y' else False
 
-    :param neo_csv_path: A path to a CSV file containing data about near-Earth objects.
-    :return: A collection of `NearEarthObject`s.
-    """
-    # TODO: Load NEO data from the given CSV file.
-    return ()
+            result.append(NearEarthObject(pdes, name, diameter, pha))
+
+    return result
 
 
 def load_approaches(cad_json_path):
-    """Read close approach data from a JSON file.
-
-    :param neo_csv_path: A path to a JSON file containing data about close approaches.
-    :return: A collection of `CloseApproach`es.
-    """
-    # TODO: Load close approach data from the given JSON file.
-    return ()
+    result = []
+    with open(cad_json_path, mode='r') as json_file:
+        approaches_dict = json.load(json_file)
+        for row in approaches_dict['data']:
+            designation = row[0]
+            time = row[3]
+            distance = float(row[4])
+            velocity = float(row[7])
+            result.append(CloseApproach(time, distance, velocity, designation))
+    return result
